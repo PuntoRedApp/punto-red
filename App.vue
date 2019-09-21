738e6fc64435f3c6e4e1f128215277103225e347
nryqgm <nryqgm@gmail.com>
2019-09-21 02:40:31 -0300

    incorporacion de archivos vue

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="container">
+		<text>Hola mundo</text>
+	</view>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style>
+.container {
+	flex: 1;
+	background-color: #fff;
+	align-items: center;
+	justify-content: center;
+}
+</style>
